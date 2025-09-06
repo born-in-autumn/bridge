6977c02d353d040a0b090bed1724af1272f6315e
Robin <1187338299@qq.com>
2025-09-06 17:28:41 +0800

    feat: adda database

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,2 +1,44 @@
-mod server;
+use std::sync::LazyLock;
+use config::{Config, ConfigError};
+use serde::Deserialize;
+use crate::config::server::ServerConfig;
+use database::DatabaseConfig;
 
+mod server;
+mod database;
+
+static CONFIG:LazyLock<AppConfig> = LazyLock::new(|| AppConfig::load().expect("Failed to init Config"));
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+    pub dev: DevConfig,
+    pub database: DatabaseConfig,
+}
+#[derive(Debug, Deserialize, Clone)]
+pub struct DevConfig {
+    pub default_redirect_url: String,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self, ConfigError> {
+        let config = Config::builder()
+            .add_source(
+                config::File::with_name("application")
+                    .format(config::FileFormat::Yaml)
+                    .required(true)
+            )
+            .add_source(
+                config::Environment::with_prefix("APP")
+                    .try_parsing(true)
+                    .separator("_")
+                    .list_separator(",")
+            )
+            .build()?;
+        config.try_deserialize()
+    }
+}
+
+pub fn get() -> &'static AppConfig {
+    &CONFIG
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,50 @@
 mod config;
+mod database;
 
-use std::{collections::{ HashMap }, sync::{Arc, RwLock} };
+use std::{collections::HashMap, sync::{Arc, RwLock}};
 
-use axum::{
-    extract::{Path, State}, http::StatusCode, response::Redirect, routing::{get, post}, Router
-};
+use axum::{extract::{Path, State}, http::StatusCode, response::Redirect, routing::{get, post}, Router};
 use uuid::Uuid;
 
 const TEMP_URL:&str =  "https://wx.mail.qq.com/home/index?sid=zTswOYxYUGYuxWo3AEZCSQAA#/list/1/1";
 
+
+#[derive(Clone)]
+pub struct AppState {
+    pub hash_map: Arc<RwLock<HashMap<String,String>>>,
+    pub redirect_url: String
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let port = config::get().server.port();
+    let redirect_url = config::get().dev.clone().default_redirect_url;
+    // println!("Using default redirect url {:?}", d);
+    // println!("Listening on port {}", c);
     let hash_map = Arc::new(RwLock::new(HashMap::<String,String>::new()));
     let app: Router = Router::new()
         .route("/{hash}", get(root))
         .route("/shorten", post(shorten_url))
-        .with_state(hash_map);
+        .with_state(AppState {
+            hash_map,
+            redirect_url,
+        });
 
-    let listenr = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listenr, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
-
-async fn root(Path(id):Path<String> ,State(map): State<Arc<RwLock<HashMap<String,String>>>>) -> Redirect {
-    println!("the id is: {}", id);
-    let map_guard = map.read().unwrap();
-    match map_guard.get(&id) {
+async fn root(Path(hash):Path<String> ,State(state): State<AppState>) -> Redirect {
+    println!("the id is: {}",hash);
+    let map_guard = state.hash_map.read().unwrap();
+    match map_guard.get(&hash) {
         Some(t) => Redirect::permanent(t),
         None => Err(StatusCode::NOT_FOUND).unwrap(),
     }
 }
 
-async fn shorten_url(State(map): State<Arc<RwLock<HashMap<String,String>>>>) -> String {
+async fn shorten_url(State(state): State<AppState>) -> String {
     let id = Uuid::new_v4();
     println!("Generated UUID: {}", id);
-    map.write().unwrap().insert(id.to_string()[..8].to_string(), TEMP_URL.to_string());
+    state.hash_map.write().unwrap().insert(id.to_string()[..8].to_string(), TEMP_URL.to_string());
     id.to_string()
 }
